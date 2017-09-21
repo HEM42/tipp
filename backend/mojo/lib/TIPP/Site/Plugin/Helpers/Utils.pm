64da5ba31a6fb8d91d8b15a991dadb64bf4f70d6
Henrik Andersen <hem@hamster.dk>
2017-09-21 22:27:35 +0200

    Changes done to original code retrofittet

--- backend/mojo/lib/TIPP/Site/Plugin/Helpers/Utils.pm
+++ backend/mojo/lib/TIPP/Site/Plugin/Helpers/Utils.pm
@@ -37,13 +37,13 @@ sub register
 
             if ( $c->{net} =~ /^10\.|^172\.|^192\.168\.|^100\./ ) {
                 if ( $c->{net} =~ /^10\./ ) {
-                    $c->{private} = 1;
+                    $c->{private} = 'RFC1918';
                 } elsif ( $c->{net} =~ /^172\.(\d+)\./ && $1 >= 16 && $1 <= 31 ) {
-                    $c->{private} = 1;
+                    $c->{private} = 'RFC1918';
                 } elsif ( $c->{net} =~ /^192\.168\./ ) {
-                    $c->{private} = 1;
+                    $c->{private} = 'RFC1918';
                 } elsif ( $c->{net} =~ /^100\.(\d+)\./ && $1 >= 64 && $1 <= 127 ) {
-                    $c->{private} = 1;
+                    $c->{private} = 'RFC6598';
                 }
             }
         }
